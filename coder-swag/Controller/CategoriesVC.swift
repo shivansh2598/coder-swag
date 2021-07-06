28400bc1ea5e487cb63f05cfe8b5e1a6808e29f9
shivansh <shivanshsinha2598@gmail.com>
2021-07-06 09:10:50 +0530

    project setup

---
+++ coder-swag/Controller/CategoriesVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Shivansh Sinha on 06/07/21.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
